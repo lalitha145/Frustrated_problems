00f26f76a567e0219094b13b1bf4a09a7e62ecb0
Chintha Lalitha <97296095+lalitha145@users.noreply.github.com>
2025-03-09 13:16:28 +0530

    alternating-groups.java

---
+++ alternating-groups.java
@@ -0,0 +1,20 @@
+// leetcode: https://leetcode.com/problems/alternating-groups-ii/description/
+
+
+
+class Solution {
+    public int numberOfAlternatingGroups(int[] colors, int k) {
+        int ans=0;
+        int n=colors.length;
+        int  cnt=1;
+        for(int i=1;i<n+k-1;i++){
+           if(colors[(i-1)%n]!=colors[i%n]) cnt++;
+           else{
+            cnt=1;
+           }
+           if(cnt>=k) ans++;
+        }
+        return ans;
+    }
+
+}
